ed84b8c9595f58e3a6021b4f79bebd336cbdd957
LaercioMatheus <154562692+LaercioMatheus@users.noreply.github.com>
2024-10-17 17:35:06 -0300

    Esse é o backup do sistema do A2.
    Esse é o backup do sistema que criei para a avaliação A2 de LTP-III onde eu tive que fazer um sistema de cadastro, listagem, exclusão, atualização, login e sessão de pessoas. Basicamente o CRUD. Esse eu tenho que completar ele mas está funcionando corretamente.

--- bancoa2.sql
+++ bancoa2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16/10/2024 às 03:04
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 17-Out-2024 às 22:34
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `registropessoas`
+-- Estrutura da tabela `registropessoas`
 --
 
 CREATE TABLE `registropessoas` (
@@ -33,10 +33,10 @@ CREATE TABLE `registropessoas` (
   `email` varchar(255) NOT NULL,
   `senha` varchar(11) NOT NULL,
   `telefone` int(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Despejando dados para a tabela `registropessoas`
+-- Extraindo dados da tabela `registropessoas`
 --
 
 INSERT INTO `registropessoas` (`id`, `nome`, `email`, `senha`, `telefone`) VALUES
@@ -45,28 +45,29 @@ INSERT INTO `registropessoas` (`id`, `nome`, `email`, `senha`, `telefone`) VALUE
 (4, 'Mateus', 'matheus@silva.com', '13', 612030405),
 (6, 'Matheus Silva', '123@matheus.com', '15', 625497130),
 (7, 'Rodrigo Gomes', 'rodrigo@gmail.com', '16', 335039706),
-(8, 'João Gomes', 'joao@gomes.com', '17', 147536398);
+(8, 'João Gomes', 'joao@gomes.com', '17', 147536398),
+(9, 'Administrador', 'admin@gmail.com', '1010', 909032644);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `registropessoas`
+-- Índices para tabela `registropessoas`
 --
 ALTER TABLE `registropessoas`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `Id_UNIQUE` (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `registropessoas`
 --
 ALTER TABLE `registropessoas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
